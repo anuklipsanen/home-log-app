7b8402c3a5362beb587950a80ce31666b182b06d
Anu <anu.k.lipsanen@gmail.com>
2026-07-07 08:06:58 +0300

    aloitettu urheilusuoritusten tuonnin lisäys

--- app/sports/page.tsx
+++ app/sports/page.tsx
@@ -1,9 +1,7 @@
-"use client";
-
-export default function SportsImportPage() {
+export default function SportsPage() {
   return (
     <main className="p-6">
-      <h1>Urheilusuorituksen tuonti</h1>
+      <h1>Urheilu</h1>
     </main>
   );
 }
